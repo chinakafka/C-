a146bdbb0017f8eb62cc6f4c0706487018cd6e32
kafka <1298441201@qq.com>
2019-05-09 20:52:03 +0800

    Create callfunction.cpp

---
+++ callfunction.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int DemoConsoleOutput();
+
+int main()
+{
+	DemoConsoleOutput();
+
+	return 0;
+}
+
+int DemoConsoleOutput()
+{
+	cout<<"This is a simple string literal"<<endl;
+	cout<<"Writing number five:"<<5<<endl;
+	cout<<"Performing division 10 / 5 ="<<10/5<<endl;
+	
+	return 0;
+}
